f33d3800e40e583b1b335dbb0ae4e689e5c0950b
file778 <file778@gmail.com>
2015-04-16 19:06:32 +0200

    Dodanie pliku FF

---
+++ src/FF.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lab702-08
+ */
+public class FF {
+    public void FF(){
+    System.out.println("asdas");
+    }
+    
+    
+}
